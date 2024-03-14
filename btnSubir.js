8f0d86b571abff37615058cb25702d607e78ee2b
Lluna <compifortnite@gmail.com>
2024-03-14 20:16:44 +0100

    js boton subir

---
+++ btnSubir.js
@@ -0,0 +1,14 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("scrollBtn").style.display = "block";
+    } else {
+        document.getElementById("scrollBtn").style.display = "none";
+    }
+}
+
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
