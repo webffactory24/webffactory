81f6d86d53036b1d5d65315144ca26570972fe62
Lluna <compifortnite@gmail.com>
2024-03-26 20:44:03 +0100

    Link fotos dni y puesta de estructura de todos los integrantes

---
+++ scripts/integrantes.js
@@ -0,0 +1,21 @@
+function LlunaInfo() {
+    window.location.href = '#infoLluna';
+}
+function RaulInfo() {
+    window.location.href = '#infoRaul';
+}
+function DavidInfo() {
+    window.location.href = '#infoDavid';
+}
+function JosueInfo() {
+    window.location.href = '#infoJosue';
+}
+function DiegoInfo() {
+    window.location.href = '#infoDiego';
+}
+function PauInfo() {
+    window.location.href = '#infoPau';
+}
+function RoberInfo() {
+    window.location.href = '#infoRober';
+}
